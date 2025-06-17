e191d3d0bf735928a4a0728ff6cdf1129074c140
AMRITESH-MISHRA-15 <amriteshmishra2004@gmail.com>
2025-06-17 15:05:26 +0530

    Schema Validations

--- backend/src/main.js
+++ backend/src/main.js
@@ -19,6 +19,60 @@ app.post("/signup",async(req,res)=>{
   }
 });
 
+app.get("/user",async(req,res)=>{
+  const userEmail = req.body.emailId;
+  try{
+    const user = await User.find({emailId:userEmail});
+    if(user.length===0){
+      res.status(404).send("User not found");
+    }
+    else{
+      res.send(user);
+    }
+  } catch{
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async(req,res)=>{
+  const userId = req.body.userId;
+  try{
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully.");
+  } catch(err){
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.patch("/user",async(req,res)=>{
+  const userId = req.body.userId;
+  const data=req.body;
+  try{
+    const user= await User.findByIdAndUpdate(userId,data,{
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User data updated successfully");
+  } catch(err){
+    res.status(400).send(err.message);
+  }
+})
+
+// Feed API - GET/feed - gets all the users from the database
+app.get("/feed",async (req,res)=>{
+  try{
+    const users=await User.find({});
+    res.send(users);
+  } catch(err){
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.use("/",(req,res)=>{
+  res.send("Hello world from the server...");
+});
+
 connectDB()
   .then(()=>{
     console.log("Database connection established...");
@@ -28,5 +82,9 @@ connectDB()
   })
   .catch((err)=>{
     console.error("Database cannot be connected !!");
+    console.error("❌ MongoDB connection failed:");
+    console.error("Error Name:", err.name);
+    console.error("Error Message:", err.message);
+    console.error("Full Error:", err);
   });
 
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,23 +2,60 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: String
+    type: String,
+    required: true,
+    trim:true,
+    minlength:2,
+    maxlength:50,
   },
   lastName: {
-    type: String
+    type: String,
+    trim:true,
+    minlength:2,
+    maxlength:50,
   },
   emailId: {
-    type: String
+    type: String,
+    required: true,
+    unique : true,
+    lowercase: true,
+    trim: true,
   },
   password: {
-    type: String
+    type: String,
+    required: true,
+    minlength:8,
+    maxlength:100,
   },
   age: {
-    type: String
+    type: Number,
+    min: 18,
+    max:120,
   },
   gender: {
-    type: String
+    type: String,
+    lowercase: true,
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new Error("Gender data is not valid");
+      }
+    }
+  },
+  photoUrl :{
+    type: String,
+    default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD116U9ZCk8bEaanCeB5rSCC2uqY5Ka_2_EA&s",
+  },
+  about:{
+    type: String,
+    maxlength:300,
+    default: "This is a default about of the user",
+  },
+  skills:{
+    type: [String]
   }
+},
+{
+  timestamps: true,
 });
 
 const User = mongoose.model("User", userSchema);
