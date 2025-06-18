af399886007cfc9a46b4074792a73bbde6e17cd6
AMRITESH-MISHRA-15 <amriteshmishra2004@gmail.com>
2025-06-19 02:21:38 +0530

    Added cookies & JWT Auth

--- backend/src/main.js
+++ backend/src/main.js
@@ -5,8 +5,13 @@ const PORT= 3000;
 const User = require("./models/user");
 const {validateSignUpData}=require("./utils/validation");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middlewares/auth");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup",async(req,res)=>{
   try{
@@ -40,9 +45,15 @@ app.post("/login",async(req,res)=>{
       throw new Error("Invalid Credentials");
     }
 
-    const isPasswordValid = await bcrypt.compare(password,user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if(isPasswordValid){
+      const token = await user.getJWT();
+
+      res.cookie("token",token,{
+        expires : new Date(Date.now()+ 8*3600000),
+      });
+
       res.send("Login Successful..");
     }
     else{
@@ -52,6 +63,22 @@ app.post("/login",async(req,res)=>{
   } catch(err){
     res.status(400).send(err.message);
   }
+});
+
+app.get("/profile", userAuth ,async(req,res)=>{
+  try{
+    const user = req.user;
+    res.send(user);
+  } catch(err){
+    res.status(400).send(err.message);
+  }
+})
+
+app.post("/sendConnectionRequest", userAuth ,async(req,res)=>{
+  const user = req.user;
+  console.log("Sending a Connection Request");
+
+  res.send(user.firstName + " sent the connection request");
 });
 
 app.get("/user",async(req,res)=>{
---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async(req,res,next)=>{
+  // read the token from the req cookies
+  try{
+    const token = req.cookies.token;
+    if(!token){
+      throw new Error("No Token Provided");
+    }
+    const decodedObj = jwt.verify(token,"devTinder@123");
+    const {_id} = decodedObj;
+    const user = await User.findById(_id);
+    if(!user){
+      throw new Error("User not found");
+    }
+    req.user= user;
+    next();
+  } catch(err){
+    res.status(400).send("Error : "+ err.message);
+  }
+};
+
+module.exports ={
+  userAuth,
+}
