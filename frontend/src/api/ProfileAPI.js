d5186f99a5014b311f0604a27d9f62a6edc2e48b
VishalMinj <vishalminj667@gmail.com>
2025-07-13 02:12:27 +0530

    login bug fix

--- frontend/src/api/ProfileAPI.js
+++ frontend/src/api/ProfileAPI.js
@@ -1,20 +1,15 @@
 
 const ProfileAPI = async() => {
-    const token = localStorage.getItem("token");
     try {
         const response = await fetch("/profile/view", {
           method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
           credentials: "include",
         });
         
         if(!response.ok){
             throw new Error("Failed to fetch user details");
         }
-        return response.json();
+        return await response.json();
     } catch (error) {
         console.log(error);
         throw error
