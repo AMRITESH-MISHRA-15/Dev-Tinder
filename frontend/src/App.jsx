8edcc96f68410213dfe165df4da15fbfb5b93a20
VishalMinj <vishalminj667@gmail.com>
2025-08-10 00:16:09 +0530

    Setting statemanagement + login functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import Login from "@components/Login";
 import Body from "@components/Body";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import appStore from "@utils/appStore";
+import { Provider } from "react-redux";
+import Feed from "@components/Feed";
 
 function App() {
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<p>Profile page</p>} />
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={appStore}>
+      <Router basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route index element={<Feed />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<p>Profile page</p>} />
+          </Route>
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@assets": path.resolve(__dirname, "./src/assets"),
     },
