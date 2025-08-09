49dea2a2b2353c1bf54db478b99567f581d2feab
VishalMinj <vishalminj667@gmail.com>
2025-08-10 00:40:42 +0530

    Added session persistance

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "@utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "@utils/userSlice"; // Add this import
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      // Fix: Use err.response.status instead of err.status
+      if (err.status === 400 || err.status === 401) {
+        navigate("/login");
+      }
+      console.error("Fetch user error:", err.response?.data || err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Navbar />
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "@utils/userSlice";
 import { useNavigate } from "react-router-dom";
-import { BASEURL } from "@utils/constants";
+import { BASE_URL } from "@utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
@@ -14,7 +14,7 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        BASEURL+"/login",
+        BASE_URL+"/login",
         {
           emailId,
           password,
