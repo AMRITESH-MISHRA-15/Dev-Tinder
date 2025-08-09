e67877a18adac799df8b648568a87acd54737874
VishalMinj <vishalminj667@gmail.com>
2025-08-09 23:24:22 +0530

    Adding router config + footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import Navbar from "@components/Navbar";
+import Body from "@components/Body";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-     <Navbar />
-    </>
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="login" element={<p>Login page</p>} />
+          <Route path="profile" element={<p>Profile page</p>} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
